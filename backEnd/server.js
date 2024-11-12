03f11f09e6bffb204e542352f7dd9a18473093e4
Gabriel W <gabriel.w@unoesc.edu.br>
2024-11-11 21:39:40 -0300

    arquivos iniciais para conexão teste com backend

---
+++ backEnd/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/teste', (request, response) => {
+    response.status(200).send('Requisição recebida.');
+});
+
+app.listen(3000);
